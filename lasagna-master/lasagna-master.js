c90e413a77fae76ef080c8f36f0ecee75ec36315
Alexis <miravite.alexis@gmail.com>
2022-11-10 10:32:06 +0800

    downloaded fruit picker exercise

--- lasagna-master/lasagna-master.js
+++ lasagna-master/lasagna-master.js
@@ -10,3 +10,38 @@
  *   ...
  * }
  */
+
+export function cookingStatus(remainingTime){
+    return remainingTime === undefined ? 'You forgot to set the timer.': remainingTime > 0 ? 'Not done, please wait.' : 'Lasagna is done.'; 
+}
+
+export function preparationTime(layers, layerPrepTime = 2) {
+    return layers.length * layerPrepTime;
+}
+
+export function quantities(layers) {
+    let noodles = 0;
+    let sauce = 0;
+    for(let layer of layers){
+        if(layer === 'noodles'){
+            noodles++;
+        }
+        if(layer === 'sauce') {
+            sauce++;
+        }
+    }
+    return {noodles: noodles * 50, sauce: sauce * 0.2}
+}
+
+export function addSecretIngredient(friendsList, myList) {
+    let secretIngredient = friendsList[friendsList.length - 1];
+    myList.push(secretIngredient);
+}
+
+export function scaleRecipe(recipe, noOfServings) {
+    let scaledRecipe = {};
+    for(let ingredient in recipe) {
+        scaledRecipe[ingredient] = recipe[ingredient] * (noOfServings/2);
+    }
+    return scaledRecipe;
+}
